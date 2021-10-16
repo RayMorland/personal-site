a77c519f977b67707ae3e996b6c3c5b2731e261f
RayMorland <raymondmorland@gmail.com>
2021-10-15 21:53:52 -0700

    work on things and stuff

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { rmAnimations } from 'src/app/shared/animations/rm-animations';
-import { ProjectService } from 'src/app/shared/services/project.service';
+import { Component, OnInit } from "@angular/core";
+import { rmAnimations } from "src/app/shared/animations/rm-animations";
+import { ProjectService } from "src/app/shared/services/project.service";
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.sass'],
-  animations: [rmAnimations]
+  selector: "app-portfolio",
+  templateUrl: "./portfolio.component.html",
+  styleUrls: ["./portfolio.component.sass"],
+  animations: [rmAnimations],
 })
 export class PortfolioComponent implements OnInit {
   projects: any;
 
-  constructor(private projectService: ProjectService) { }
+  constructor(private projectService: ProjectService) {}
 
   ngOnInit(): void {
-    this.projectService.GetProjects().subscribe(res => {
+    this.projectService.GetProjects().subscribe((res) => {
+      console.log(res);
       this.projects = res;
     });
   }
-
 }
