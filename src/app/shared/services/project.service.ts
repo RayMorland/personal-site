a669c0f60fae23d9e28663b8ada0d82358f69a89
RayMorland <raymondmorland@gmail.com>
2021-10-24 17:39:09 -0700

    Work on strapi integration

--- src/app/shared/services/project.service.ts
+++ src/app/shared/services/project.service.ts
@@ -1,20 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProjectService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public GetProjects(): Observable<any> {
-    return this.http.get('http://localhost:1337/projects')
+    return this.http.get("https://rocky-thicket-98404.herokuapp.com/projects");
   }
 
   public GetProject(slug: string): Observable<any> {
-    const params = new HttpParams().append('slug', slug);
-    return this.http.get('http://localhost:1337/projects', {params: params});
+    const params = new HttpParams().append("slug", slug);
+    return this.http.get("http://localhost:1337/projects", { params: params });
   }
 }
