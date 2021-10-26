0f77e298efe6c6bdd02fc91bc191ac1e8b85c4e3
RayMorland <raymondmorland@gmail.com>
2021-10-26 08:52:38 -0700

    Work on integrating strapi images

--- src/app/pages/portfolio-project/portfolio-project.component.ts
+++ src/app/pages/portfolio-project/portfolio-project.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { rmAnimations } from 'src/app/shared/animations/rm-animations';
-import Project from 'src/app/shared/models/project.model';
-import { BlogpostService } from 'src/app/shared/services/blogpost.service';
-import { ProjectService } from 'src/app/shared/services/project.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { rmAnimations } from "src/app/shared/animations/rm-animations";
+import Project from "src/app/shared/models/project.model";
+import { BlogpostService } from "src/app/shared/services/blogpost.service";
+import { ProjectService } from "src/app/shared/services/project.service";
 
 @Component({
-  selector: 'app-portfolio-project',
-  templateUrl: './portfolio-project.component.html',
-  styleUrls: ['./portfolio-project.component.sass'],
+  selector: "app-portfolio-project",
+  templateUrl: "./portfolio-project.component.html",
+  styleUrls: ["./portfolio-project.component.sass"],
   animations: [rmAnimations],
 })
 export class PortfolioProjectComponent implements OnInit {
-  project: Project = { title: '', services: '', slug: '', description: '' };
+  project: any = { title: "", services: "", slug: "", description: "" };
+  images: any = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -21,11 +22,14 @@ export class PortfolioProjectComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
-      const id = params['id'];
-      console.log('Url Id: ', id);
-      this.projectService
-        .GetProject(id)
-        .subscribe((res) => (this.project = res[0]));
+      const id = params["id"];
+      console.log("Url Id: ", id);
+      this.projectService.GetProject(id).subscribe((res) => {
+        console.log(res[0]);
+        this.project = res[0];
+        this.images = this.project.primary_image;
+        console.log(this.images[0].url);
+      });
     });
   }
 }
