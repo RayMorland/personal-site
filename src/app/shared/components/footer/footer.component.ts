347f1ff4a266890b56df30ceb3d56a53e2dbf940
RayMorland <raymondmorland@gmail.com>
2021-11-16 12:09:34 -0800

    Work on footer animations

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,11 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import { Observable, Subscription } from "rxjs";
 import { filter } from "rxjs/operators";
@@ -9,11 +16,21 @@ import { Event } from "@angular/router";
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.sass"],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit {
   // private routerEventSub: Subscription;
   windowWidth: any;
+  @ViewChild("footer") footer: any;
   @HostListener("window:resize", ["$event"]) onResize(): void {
     this.windowWidth = window.innerWidth;
+  }
+  @HostListener("window:scroll", ["$event"]) onScroll(event: any) {
+    if (
+      this.element.nativeElement.getBoundingClientRect().y <=
+      window.innerHeight * 0.75
+    ) {
+      this.footer.nativeElement.style.opacity = 1;
+      this.footer.nativeElement.style.transform = "translateY(0px)";
+    }
   }
 
   public backgroundColor: string = "none";
@@ -21,16 +38,30 @@ export class FooterComponent implements OnInit {
   route: any;
   color: any;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private element: ElementRef) {
     this.routerEventSub = router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((routeChange: any) => {
         console.log(routeChange);
         this.changeColor(routeChange);
+        if (this.footer) {
+          this.footer.nativeElement.style.opacity = 0;
+          this.footer.nativeElement.style.transform = "translateY(100px)";
+          this.footer.nativeElement.style.transition =
+            "opacity 1s ease, transform 1s ease";
+        }
       });
   }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    console.log(this.footer);
+    this.footer.nativeElement.style.opacity = 0;
+    this.footer.nativeElement.style.transform = "translateY(100px)";
+    this.footer.nativeElement.style.transition =
+      "opacity 1s ease, transform 1s ease";
+  }
 
   changeColor(routeChange: any) {
     if (
